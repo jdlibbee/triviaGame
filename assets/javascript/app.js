fec2acfdf84d1490c1667708c0b8b860d0a4b009
jdlibbee <libbeelibbee@yahoo.com>
2018-05-17 13:40:45 -0600

    adding code

---
+++ assets/javascript/app.js
@@ -0,0 +1,50 @@
+What is the name of the games / groundskeeper of Hogwarts ?
+    Hagrid
+Hagar
+Hordid
+Hortencia
+What kind of creature is Buckbeak ?
+    Phoenix
+Hippogriff
+Dragon
+Owl
+What house is supposed to be super smart ?
+    Catwhisker
+Hufflepuff
+Ravenclaw
+Dogtale
+What book did Newt Scamander write ?
+    Care for Magical Creatures
+Fantastic Beasts and Where to Find Them
+Delores Umbridge & Why She’s Got to Go!
+Hogwarts A History
+At the end of the first movie, Dumbledore gets what flavor of jelly bean ?
+    Barf
+Earwax
+Buttered toffee
+Toasted marshmallow
+What charm do you use for levitation ?
+    Windgardium Leviosa
+Accio
+Alohamora
+Lumos
+Who teaches Herbology ?
+    Ferenz 
+Flitwick
+Sprout
+Hooch
+What is the name of Hagrid’s Spider ?
+    Aragog
+Aragorn
+Arathorn
+Aralia
+All of the following are a Horcurx, except ?
+    Professor Quirrel
+Harry Potter
+Womping Willow
+Locket
+What is the creature that comes out of a young wizard not using their power ?
+    Basilisk
+Thestral
+Swooping Evil
+Obscurial
